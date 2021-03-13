c58de8b9361f11cb78e9059baa39d6c2210af67d
vsbdn <v.boldurean@gmail.com>
2021-03-13 18:13:19 +0000

    fix printer.js, modified

--- src/printer.js
+++ src/printer.js
@@ -1,7 +1,3 @@
 import fs from 'fs';
 
-export default (filepath) => fs.readFile(
-  filepath,
-  'utf-8',
-  (_err, data) => console.log(data),
-);
+export default (filepath) => fs.readFile(filepath, 'utf-8', (_err, data) => console.log(data));
